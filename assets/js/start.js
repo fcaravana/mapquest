77e99f4def46015843067aafa3f03da891038ad2
Francisco Caravana <fcaravana@gmail.com>
2015-10-07 23:35:55 +0100

    Making some csv fields adjustments and write start javascript code.

--- assets/js/start.js
+++ assets/js/start.js
@@ -1,3 +1,57 @@
 /**
  * App scope.
  */
+var APP = {
+    base: function() {
+        var _location = window.location.pathname;
+        var _directory = _location.substring(0, _location.lastIndexOf('/'));
+        
+        return _directory;
+    },
+    skipCache: true,
+    serial: '20151007230000',
+    loadedModules: {}, 
+    start: function() {
+
+        APP.loadedModules.helpers = new APP.helpers();
+        APP.loadedModules.dataController = new APP.dataController();
+
+        APP.loadedModules.dataController.loadData.then(function() {
+            APP.loadedModules.mapController = new APP.mapController();
+            APP.loadedModules.mapController.init();
+        }, function(error) {
+            alert(error);
+        });
+
+    }
+
+};
+
+/**
+ * Load required libraries and start.
+ */
+basket.require(
+    {url: APP.base() + '/assets/js/bower_components/jquery/dist/jquery.min.js?s=' + APP.serial, skipCache: APP.skipCache},
+    {url: APP.base() + '/assets/js/bower_components/bootstrap/dist/js/bootstrap.min.js?s=' + APP.serial, skipCache: APP.skipCache},
+    {url: APP.base() + '/assets/js/bower_components/bootstrap-table/dist/bootstrap-table.min.js?s=' + APP.serial, skipCache: APP.skipCache},
+    {url: APP.base() + '/assets/js/bower_components/csvjson.js/csvjson.min.js?s=' + APP.serial, skipCache: APP.skipCache},
+    {url: APP.base() + '/assets/js/bower_components/gmaps/gmaps.min.js?s=' + APP.serial, skipCache: APP.skipCache}
+).then(function() {
+
+    $(document).ready(function() {
+        basket.require(
+            {url: APP.base() + '/assets/js/app/helpers/helpers.js?s=' + APP.serial, skipCache: APP.skipCache},
+            {url: APP.base() + '/assets/js/app/controllers/dataController.js?s=' + APP.serial, skipCache: APP.skipCache},
+            {url: APP.base() + '/assets/js/app/controllers/mapController.js?s=' + APP.serial, skipCache: APP.skipCache}
+        ).then(function() {
+
+            /**
+             * Start app.
+             */
+            APP.start();
+
+        });
+
+    });
+
+});
